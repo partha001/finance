542106215eb1fbff07fe8248a67798ca9c1ea4a7
partha-desktop\partha <biswas.partha1@gmail.com>
2022-08-15 01:37:32 +0530

    adding portfolio-manager-ui

--- portfolio-manager-ui/src/App.js
+++ portfolio-manager-ui/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import "./components/Header"
+import "./components/Navigation"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="container jumbotron bg-secondary rounded">      
+          <h1>Portfolio Manager</h1>
+      </div>
+
+      <Navigation></Navigation>
+
+
+      <div class="container footerStyle bg-secondary rounded">
+        <div class="footerStyleContent">
+          <p class="text-white credit">Developed by:
+            Partha Biswas
+            <a href="#">Partha Biswas</a>
+            . &copy; Copyright 2018</p>
+        </div>
+      </div>
     </div>
   );
 }
