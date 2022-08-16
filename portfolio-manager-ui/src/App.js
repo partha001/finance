5596c131ac9d08207952e03d8591db6c11e23c46
partha-desktop\partha <biswas.partha1@gmail.com>
2022-08-16 11:34:50 +0530

    navigation changes

--- portfolio-manager-ui/src/App.js
+++ portfolio-manager-ui/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import "./components/Header"
 import "./components/Navigation"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
@@ -9,16 +8,16 @@ import Navigation from './components/Navigation';
 function App() {
   return (
     <div>
-      <div class="container jumbotron bg-secondary rounded">      
+      <div className="container jumbotron bg-secondary rounded">      
           <h1>Portfolio Manager</h1>
       </div>
 
-      <Navigation></Navigation>
+      <Navigation activeMenu="menu1"></Navigation>
 
 
-      <div class="container footerStyle bg-secondary rounded">
-        <div class="footerStyleContent">
-          <p class="text-white credit">Developed by:
+      <div className="container footerStyle bg-secondary rounded">
+        <div className="footerStyleContent">
+          <p className="text-white credit">Developed by:
             Partha Biswas
             <a href="#">Partha Biswas</a>
             . &copy; Copyright 2018</p>
--- portfolio-manager-ui/src/components/Navigation.js
+++ portfolio-manager-ui/src/components/Navigation.js
@@ -1,59 +1,78 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React, { useState } from 'react';
+import '../components/Menu1';
+import Menu1 from './Menu1';
 
-function Navigation() {
+function Navigation(props) {
 
-    function getDividend() {
-        fetch("http://localhost:8081/dividendSummaryByYear")
-        .then((response) =>{
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-        })
+    const [activeMenu,setActiveMenu] = useState(props.activeMenu);
+
+    
+
+    const menuClickHandler = (event)=>{
+        console.log(event.target.id);
+        setActiveMenu(event.target.id);   
     }
 
+    let myvar1 = (activeMenu==='' && <p>menu1</p>);
+    myvar1 = (activeMenu==='menu2' && <p>menu2</p>);
+    myvar1 = (activeMenu==='menu2' && <p>yearlySummary</p>);
+
+    // function getDividend() {
+    //     fetch("http://localhost:8081/dividendSummaryByYear")
+    //         .then((response) => {
+    //             return response.json();
+    //         })
+    //         .then((data) => {
+    //             console.log(data);
+    //         })
+    // }
+
     return (<div>
-        <div class="container rounded">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+        <div className="container rounded">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+
+                    {/* <a class="navbar-brand" href="#">Home</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </button> */}
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#" id="menu1" onClick={menuClickHandler}>Menu1</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                            <li className="nav-item" >
+                                <a className="nav-link" href="#"  id="menu2" onClick={menuClickHandler}>Menu2</a>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Dividends
                                 </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Yearly Summary</a></li>
-                                    <li><a class="dropdown-item" href="#">Quarterly Summary</a></li>
-                                    <li><a class="dropdown-item" href="#">Year and Quarterly Summary</a></li>
-                                    <li><a class="dropdown-item" href="#">Equitywise Summary</a></li>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#"  id="menuYearlySummar" onClick={menuClickHandler}>Yearly Summary</a></li>
+                                    <li><a className="dropdown-item" href="#"  id="menuQuarterlySummary" onClick={menuClickHandler}>Quarterly Summary </a></li>
+                                    <li><a className="dropdown-item" href="#"  id="menuYearlyQuarterlySummary" onClick={menuClickHandler}>Year and Quarterly Summary</a></li>
+                                    <li><a className="dropdown-item" href="#"  id="menuEquitySummary" onClick={menuClickHandler}>Equitywise Summary</a></li>
                                 </ul>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
                             </li>
                         </ul>
-                        <form class="d-flex">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex">
+                            <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
             </nav>
         </div>
-        <div class="container rounded">
 
-            hello world
+
+        <div className="container rounded">
+         {myvar1}
         </div>
     </div>)
 }
