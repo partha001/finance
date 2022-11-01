ded4242f63ae8aa11a46ef3d072c32c283668239
partha <biswaspa@vmware.com>
2022-11-02 01:54:03 +0530

    adding some logs

--- portfolio-manager/src/main/java/com/portfoliomanger/service/DividendService.java
+++ portfolio-manager/src/main/java/com/portfoliomanger/service/DividendService.java
@@ -2,6 +2,8 @@ package com.portfoliomanger.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +17,13 @@ import com.portfoliomanger.util.MathUtil;
 @Service
 public class DividendService {
 		
+	private static final Logger logger = LoggerFactory.getLogger(DividendService.class);
 	
 	@Autowired
 	private DividendDao dividendDao;
 	
-	public ResponseEntity<DividendSummaryResponse> getDividendSummarByYear(){        
+	public ResponseEntity<DividendSummaryResponse> getDividendSummarByYear(){
+		logger.info("inside DividendService.getDividendSummarByYear()");
 		List<DividendDto> list = dividendDao.getDividendSummarByYear();	
 		list.forEach(item -> {
 			item.setAvgAmount(MathUtil.roundDouble(item.getAvgAmount()));
@@ -33,6 +37,7 @@ public class DividendService {
 	
 	
 	public ResponseEntity<DividendSummaryResponse> getDividendSummarByQuarter(){
+		logger.info("inside DividendService.getDividendSummarByQuarter()");
 		List<DividendDto> list = dividendDao.getDividendSummarByQuarter();	
 		list.forEach(item -> {
 			item.setAvgAmount(MathUtil.roundDouble(item.getAvgAmount()));
@@ -46,6 +51,7 @@ public class DividendService {
 	
 	
 	public ResponseEntity<DividendSummaryResponse> getDividendSummarByYearAndQuarter(){
+		logger.info("inside DividendService.getDividendSummarByYearAndQuarter()");
 		List<DividendDto> list = dividendDao.getDividendSummarByYearAndQuarter();
 		list.forEach(item -> {
 			item.setAvgAmount(MathUtil.roundDouble(item.getAvgAmount()));
@@ -58,6 +64,7 @@ public class DividendService {
 
 
 	public ResponseEntity<DividendSummaryResponse> getDividendSummarByEquity() {
+		logger.info("inside DividendService.getDividendSummarByEquity()");
 		List<DividendDto> list = dividendDao.getDividendSummarByEquity();
 		list.forEach(item -> {
 			item.setAvgAmount(MathUtil.roundDouble(item.getAvgAmount()));
@@ -70,6 +77,7 @@ public class DividendService {
 
 
 	public ResponseEntity<DividendSummaryResponse> getDividendDetails() {
+		logger.info("inside DividendService.getDividendDetails()");
 		List<DividendDto> list = dividendDao.getDividendDetails();
 		list.forEach(item -> {
 			item.setAvgAmount(MathUtil.roundDouble(item.getAvgAmount()));
