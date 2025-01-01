939fbaff16ef81fcdb8d1904fdaf744755940703
Partha Biswas <biswas.partha1@gmail.com>
2025-01-01 23:20:23 +0530

    wealth-manager: test script

---
+++ wealth-manager/wealth-manager-backend-service/src/main/resources/liquibase/changelogs/8_dml_AssetMaster.sql
@@ -0,0 +1,29 @@
+--liquibase formatted sql
+--changeset partha:8_dml_AssetMaster
+
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'zerodha_equity'     ,3316  ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'zerodha_cash'       ,23    ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'zerodha_mf'         ,1111  ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'groww_equity'       ,0     ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'groww_mf'           ,4447  ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'groww_cash'         ,0     ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'angelone_equity'    ,420   ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'angelone_cash'      ,2     ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'axis_mf'            ,1426  ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'wazir'              ,18    ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'vauld'              ,0     ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'indmoney_us_stocks' ,41    ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'lic'                ,199   ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'cash'               ,380   ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('partha',  'pf'                 ,2465  ,2024-01-01);
+
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('shibani',  'upstox_mf'         ,1102  ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('shibani',  'upstox_equity'     ,487   ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('shibani',  'upstox_cash'       ,6     ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('shibani',  'zerodha_equity'    ,591   ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('shibani',  'zerodha_cash'      ,1.5   ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('shibani',  'zerodha_mf'        ,0     ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('shibani',  'cash'              ,167   ,2024-01-01);
+insert into WealthManager.AssetMaster(username,assetName,amount,valudationDate) values ('shibani',  'goldenpi_bond'     ,801   ,2024-01-01);
+
+
